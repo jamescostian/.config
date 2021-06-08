ddab30251f18791b4f0931f0a2b8467348add200
James Costian <james@jamescostian.com>
2021-06-07 20:44:16 -0400

    Avoid some bugs with geometry

--- zsh-james/theme.zsh
+++ zsh-james/theme.zsh
@@ -4,11 +4,10 @@ GEOMETRY_GIT_SEPARATOR=" "
 GEOMETRY_SYMBOL_GIT_CLEAN="⬡"
 GEOMETRY_SYMBOL_GIT_BARE="⬡"
 GEOMETRY_SYMBOL_GIT_DIRTY="⬢"
-GEOMETRY_STATUS_COLOR_HASH=true
 GEOMETRY_PATH_COLOR=5
 GEOMETRY_GIT_NO_COMMITS_MESSAGE="❄new❄ "
 
 PS1="$ " # Temporary prompt until the real one has loaded
 zinit ice from"gh" lucid
 zinit light geometry-zsh/geometry
-GEOMETRY_STATUS_COLOR=$(geometry::hostcolor || echo 1)
+GEOMETRY_STATUS_COLOR=$(geometry::hostcolor)
