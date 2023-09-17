cb98b2dc867b96073c6fe12d955d403b5c23e029
James Costian <james@jamescostian.com>
2023-09-17 12:11:19 -0500

    Use thefuck when the command was not found

--- zsh-james/low-priority.zsh
+++ zsh-james/low-priority.zsh
@@ -3,6 +3,18 @@
 # Enable thefuck, if it's installed
 if hash thefuck 2> /dev/null; then
 	eval $(thefuck --alias)
+	command_not_found_handler () {
+		TF_SHELL_ALIASES=$(alias);
+		TF_CMD=$(
+			export TF_SHELL_ALIASES;
+			export TF_SHELL=zsh;
+			export TF_ALIAS=fuck;
+			export TF_HISTORY="$@";
+			export PYTHONIOENCODING=utf-8;
+			thefuck THEFUCK_ARGUMENT_PLACEHOLDER
+		) && eval $TF_CMD;
+		test -n "$TF_CMD" && print -s $TF_CMD;
+	}
 fi
 
 # An alias from git-shortcuts will try to overwrite these, so these alias overwrites them to what I want
